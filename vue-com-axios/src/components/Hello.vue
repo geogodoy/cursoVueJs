d55f82b17df894eecb8828de1ec3fb6c85ef837e
geogodoy <geovana.godoy12@gmail.com>
2020-09-24 10:41:58 -0300

    atualizacao de pasta

---
+++ vue-com-axios/src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="search">
+      <input v-model="username" placeholder="Enter a github username!"/>
+    </form>
+    <p v-if="data.name && data.location">
+      {{ data.name }} ({{ data.login }})
+      is from
+      {{ data.location }}!
+    </p>
+    <p v-else>{{ errorMsg }}</p>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import axios from 'axios'
+  import VueAxios from 'vue-axios'
+
+  Vue.use(VueAxios, axios)
+
+  export default {
+    data () {
+      return {
+        username: '',
+        data: [],
+        errorMsg: ''
+      }
+    },
+    methods: {
+      search () {
+        const api = `https://api.github.com/users/${this.username}`
+        Vue.axios.get(api).then(response => {
+          this.data = response.data
+          console.log(this.data)
+        }).catch(error => {
+          this.errorMsg = 'No user or no location!'
+          this.data = []
+          console.log(error)
+        })
+      }
+    }
+  }
+</script>
